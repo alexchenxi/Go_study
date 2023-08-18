1bf527be91715f963bc7a8f855c9dac81f6d8617
alexchenxi <alexchenxizhang@163.com>
2023-08-18 18:06:55 +0800

    update

---
+++ go_pro_new/src/PRO01/test.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name string
+		age  int
+		b    bool
+	)
+	name = "alex"
+	age = 30
+	b = true
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+
+}

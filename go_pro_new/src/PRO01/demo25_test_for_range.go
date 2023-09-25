e4c19540ab875f49af09dff486d3d98414a74d0c
alexchenxi <alexchenxizhang@163.com>
2023-09-25 18:06:08 +0800

    update: for range

--- go_pro_new/src/PRO01/demo25_test_for_range.go
+++ go_pro_new/src/PRO01/demo25_test_for_range.go
@@ -2,22 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-	f4_25()
+func test_for_range() {
+	f3_25()
 }
 func f1_25() {
 	var a = [...]int{12, 24, 36}
 
-	/* 	for i, v := range a {
-		fmt.Printf("i: %v:%v\n", i, v)
-	} */
-
-	for _, v := range a {
-		fmt.Printf("v:%v\n", v)
+	for i, v := range a {
+		fmt.Printf("%v:%v\n", i, v)
 	}
+
+	// for _, v := range a {
+	// 	fmt.Printf("v:%v\n", v)
+	// }
 }
 
 func f2_25() {
+	// 切片是动态的数组
 	var s = []int{1, 2, 3}
 	for i, v := range s {
 		fmt.Printf("%v:%v\n", i, v)
@@ -26,7 +27,7 @@ func f2_25() {
 
 func f3_25() {
 
-	var m = make(map[string]any, 0)
+	var m = make(map[string]any, 1)
 	m["name"] = "tom"
 	m["age"] = 20
 	m["email"] = "test@gmail.com"
